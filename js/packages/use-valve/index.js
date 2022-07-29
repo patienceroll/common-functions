ba84b23bfc1fe28a10e4e614284224aeaa92b646
zhangxianlei <zhangxianlei@uphicoo.com>
2022-07-29 15:15:14 +0800

    docs: ✏️ 完善节流函数的类型声明

--- js/packages/use-valve/index.js
+++ js/packages/use-valve/index.js
@@ -1,10 +1,14 @@
+/** ### 节流
+ * - options.during 节流时间
+ * - 会把函数的返回值通过promise返回
+ */
 export default function useValve(func, options = { during: 300 }) {
     const { during } = options;
     let lastRunTime = 0;
     return (...params) => {
         const currentTime = +new Date();
         return new Promise((resolve, reject) => {
-            const outOfValveDuring = (currentTime - lastRunTime) > during;
+            const outOfValveDuring = currentTime - lastRunTime > during;
             lastRunTime = currentTime;
             if (outOfValveDuring)
                 resolve(func(...params));
--- packages/use-valve/index.ts
+++ packages/use-valve/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type Options = {
 	/** 持续时间 ms */
 	during: number;
@@ -7,16 +8,16 @@ export type Options = {
  * - options.during 节流时间
  * - 会把函数的返回值通过promise返回
  */
-export default function useValve(
-	func: (...params: unknown[]) => unknown,
+export default function useValve<T extends (...arg: any[]) => any>(
+	func: T,
 	options: Options = { during: 300 }
 ) {
 	const { during } = options;
 	let lastRunTime = 0;
-	return (...params: Parameters<typeof func>) => {
+	return (...params: Parameters<T>) => {
 		const currentTime = +new Date();
-		return new Promise<ReturnType<typeof func>>((resolve, reject) => {
-			const outOfValveDuring = (currentTime - lastRunTime) > during;
+		return new Promise<ReturnType<T>>((resolve, reject) => {
+			const outOfValveDuring = currentTime - lastRunTime > during;
 			lastRunTime = currentTime;
 			if (outOfValveDuring) resolve(func(...params));
 			else reject();
